edc925e6affce1367cc01ca9d5dfa0352853b2ab
smn74 <sarah.friedrich@alumni.uni-ulm.de>
2018-09-17 16:04:18 +0200

    Fix bug in WildBS

--- R/MANOVA_Statistics.R
+++ R/MANOVA_Statistics.R
@@ -97,7 +97,8 @@ MANOVA.Stat<- function(data, n, hypo_matrix, iter, alpha, resampling, n.groups, 
      # MATS
      DP <- diag(SnP)*diag(p*n.groups)
      Q_N_P <- N* t(meansP)%*%t(H)%*%MASS::ginv(H%*%DP%*%t(H))%*%H%*%meansP
-     return(list(WTPS, Q_N_P, meansP, DP))
+     wbs <- list(WTPS = WTPS, Q_N_P = Q_N_P, meansP = meansP, DP = DP)
+     return(wbs)
    }
   
   cl <- makeCluster(CPU)
